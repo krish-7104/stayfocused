d7723bc84726a8abe6e3186a92894295fae7c75c
Krish Jotaniya <krishjotaniya71@gmail.com>
2023-02-01 23:17:21 +0530

    first released

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useCallback, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [date, setDate] = useState("");
+  const [fontsLoaded] = useFonts({
+    "Montserrat-Medium": require("./assets/fonts/Montserrat-Medium.ttf"),
+    "Montserrat-Regular": require("./assets/fonts/Montserrat-Regular.ttf"),
+  });
+  setTimeout(() => {
+    let date = new Date();
+    setDate(date.toLocaleTimeString());
+  }, 1000);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={styles.time}>{date.slice(0, 2)}</Text>
+      <Text style={styles.time}>{date.slice(3, 5)}</Text>
+      <Text style={styles.time}>{date.slice(6, 8)}</Text>
+      <Text style={styles.text}>stay focused</Text>
+      <StatusBar hidden />
     </View>
   );
 }
@@ -13,8 +39,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#181818",
+  },
+  time: {
+    color: "#e7e7e7",
+    fontSize: 160,
+    letterSpacing: 4,
+    fontFamily: "Montserrat-Medium",
+  },
+  text: {
+    position: "absolute",
+    bottom: 26,
+    fontSize: 22,
+    fontFamily: "Montserrat-Regular",
+    color: "#e7e7e7",
   },
 });
